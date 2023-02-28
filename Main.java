f7589eda70153f1acfa2e6fd6fe071ddeb3805ee
atul sonawane <>
2023-02-28 15:20:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main { 
+  public static void Helloworld() {
+    System.out.println("Hello world!");
+    for(int i=0; i<5; i++){
+      System.out.println("Hey Sham Good Luck For Java");
+      System.out.println("hey sham good luck for java");
+    } 
+  }
+ public static void main(String[] agrs){
+   Helloworld();
+ }
+}
